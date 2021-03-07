8038c6cc0d1e9013aeaf8262f393f10628480f79
A.Protasevich <larsplotner@gmail.com>
2021-03-07 12:25:56 +0300

    add func playRound

--- game.js
+++ game.js
@@ -8,14 +8,26 @@ const computerPlay = () =>{
     }
     // get index of max value in array
     let max_roll = rolls.indexOf(Math.max(...rolls));
+    
     switch (max_roll){
         case 0:
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         case 2:
-            return "Scissors";
+            return "scissors";
     }
 }
 
+const playRound = (playerSelection, computerSelection)=>{
+    if (playerSelection === computerSelection) {
+        return `Draw game ! You select ${playerSelection}, Computer select ${computerSelection}`;
+    } else if ((playerSelection === 'scissors' && computerSelection === 'paper')
+            || (playerSelection === 'paper' && computerSelection === 'rock')
+            || (playerSelection === 'rock' && computerSelection === 'scissors')){
+        return `You win! You select ${playerSelection}, Computer select ${computerSelection}`;
+    } else {
+        return `You lose! You select ${playerSelection}, Computer select ${computerSelection}`;
+    }
+}
 console.log(computerPlay())
